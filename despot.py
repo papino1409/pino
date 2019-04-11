7991fbad22a71b47c6bdfc8a7abe4652f6d20268
papino1409 <49399362+papino1409@users.noreply.github.com>
2019-04-11 13:28:53 +0000

    exo sur commande aleatoires

--- despot.py
+++ despot.py
@@ -1,9 +1,13 @@
-from random import*
-n= randit(0,9)
-b= input ("donner un nombre comprise entren 0 et 9)
-if a==n:
-print("felicitation")
-else:
-print("veillez saisir un nombre svp")
+mport random
+import sys
+n= random.randint(0,9)
+b= int(input("donner un nombre comprise entren 0 et 9"))
+while True:
+ if b == n:
+  print("felicitation")
+ else:
+  print("veillez saisir un nombre svp")
+  sys.exit()
+
 
 
